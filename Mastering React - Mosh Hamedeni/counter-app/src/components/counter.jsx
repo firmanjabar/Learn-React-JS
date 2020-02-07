9ab6e0e71cd9bff5551547d2adcea5e54a434790
FirmanJabar <firmanabduljabar@gmail.com>
2020-02-07 19:28:03 +0800

    :rocket: 5- Embedding Expressions

--- Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
+++ Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button className="btn btn-outline-dark">Increase +</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
   }
 }
 
