b1a0bffeb9b5b8f76971796b0d1a4f85c6295118
FirmanJabar <firmanabduljabar@gmail.com>
2020-02-07 19:51:37 +0800

    :rocket: 6- Setting Attributes

--- Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
+++ Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imgUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    backgroundColor: "pink",
+    marginTop: 10
   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button className="btn btn-outline-dark">Increase +</button>
+        <img src={this.state.imgUrl} alt={this.state.imgUrl} />
+        <div style={this.styles}>
+          <span style={{ fontSize: 13 }} className="badge badge-dark m-4">
+            {this.formatCount()}
+          </span>
+          <button className="btn btn-outline-dark btn-sm">Increase +</button>
+        </div>
       </React.Fragment>
     );
   }
