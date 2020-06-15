1b1cf9ce2311e199003d7c3e57630a19813275e4
FirmanJabar <firmanabduljabar@gmail.com>
2020-06-15 13:11:00 +0800

    :pencil: 4.3- Passing Data to Components

--- Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
+++ Mastering React - Mosh Hamedeni/counter-app/src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncrement = () => {
     this.setState({
-      count: this.state.count + 1,
+      value: this.state.value + 1,
     });
   };
 
@@ -31,13 +31,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "dark" : "success";
+    classes += this.state.value === 0 ? "dark" : "success";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
   }
 }
 
--- Mastering React - Mosh Hamedeni/counter-app/src/components/counters.jsx
+++ Mastering React - Mosh Hamedeni/counter-app/src/components/counters.jsx
@@ -7,14 +7,14 @@ export default class counters extends Component {
       { id: 1, value: 4 },
       { id: 2, value: 5 },
       { id: 3, value: 2 },
-      { id: 4, value: 1 },
+      { id: 4, value: 0 },
     ],
   };
   render() {
     return (
       <>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </>
     );
