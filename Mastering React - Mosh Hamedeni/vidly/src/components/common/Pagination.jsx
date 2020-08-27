95bee492c89466bf3eb9670211a9d93b0f6d6c1c
FirmanJabar <firmanabduljabar@gmail.com>
2020-08-27 17:24:03 +0800

    :sparkles: 5.4- Pagination- Displaying Pages

--- Mastering React - Mosh Hamedeni/vidly/src/components/common/Pagination.jsx
+++ Mastering React - Mosh Hamedeni/vidly/src/components/common/Pagination.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import _ from 'lodash';
 
-const Pagination = () => {
-  return <div></div>;
+const Pagination = (props) => {
+  const { totalMovies, pageSize } = props;
+
+  const pageCount = Math.ceil(totalMovies / pageSize);
+  if (pageCount === 1) return null;
+  const page = _.range(1, pageCount + 1);
+
+  return (
+    <nav>
+      <ul className='pagination'>
+        {page.map((numb) => (
+          <li key={numb} className='page-item'>
+            <a className='page-link'>{numb}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
